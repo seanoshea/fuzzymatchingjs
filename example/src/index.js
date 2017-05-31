7167447cb250f6208bcce3886fba5cd806ffd4b8
seanoshea <sean.oshea@americanwell.com>
2017-05-31 16:58:12 -0400

    Trying to access the local version of the fuzzy matching lib.

--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FuzzyMatchingJS from 'fuzzymatchingjs';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+console.warn('Do I have access to it?', FuzzyMatchingJS);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
