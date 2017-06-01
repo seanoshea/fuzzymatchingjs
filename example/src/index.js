c8631884e753149bb95f887a8c9ce53e41b6ad64
seanoshea <sean.oshea@americanwell.com>
2017-06-01 15:18:55 -0400

    An Example.

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FuzzyMatchingJS from 'fuzzymatchingjs';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as FuzzyMatchingJS from 'fuzzymatchingjs';
 import './index.css';
-
-console.warn('Do I have access to it?', FuzzyMatchingJS);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
