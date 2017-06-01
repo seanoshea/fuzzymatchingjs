e45a40dcd746a201b6c72e9053509a9306b934d8
seanoshea <sean.oshea@americanwell.com>
2017-06-01 10:13:16 -0400

    Docs

--- src/lib/fuzzyMatchPattern.js
+++ src/lib/fuzzyMatchPattern.js
@@ -5,9 +5,16 @@ function matchBitapOfText(text, pattern, loc, options) {
   return 1;
 }
 
+/**
+ * @arg {String} text - the text to search through for the pattern
+ * @arg {String} pattern - the pattern within the text to search for
+ * @arg {String} loc - where in the text to start the search
+ * @arg {Object} options
+ * @arg {String} [options.xyz] - some additional options.
+ */
 export default function fuzzyMatchPattern(text, pattern, loc, options) {
   if (text == null || pattern == null || loc == null) {
-    throw new Error('Null input. (fuzzyMatchPattern)');
+    throw new Error("Null input. (fuzzyMatchPattern)");
   }
   const location = Math.max(0, Math.min(loc, text.length));
   if (text === pattern) {
