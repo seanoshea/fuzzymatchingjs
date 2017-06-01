df040979e73b3d12104a08f418fc783ed0359e97
seanoshea <sean.oshea@americanwell.com>
2017-06-01 15:57:05 -0400

    A very simple example.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as FuzzyMatchingJS from 'fuzzymatchingjs';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      pattern: "",
+      result: "",
+    };
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.setState({result: FuzzyMatchingJS.fuzzyMatchPattern(this.state.text, this.state.pattern, 0)});
+  }
+
+  handleTextChange = (e) => {
+    this.setState({text: e.target.value});
+  }
+
+  handlePatternChange = (e) => {
+    this.setState({pattern: e.target.value});
+  }
+
   render() {
     return (
-      <div className="App">Fuzzy Matching JavaScript Example Application</div>
+      <div className="App">
+        <p>Fuzzy Matching JavaScript Example Application</p>
+        <form>
+          <fieldset>
+            <label htmlFor="text">Text</label>
+            <input type="text" name="text" id="text" value={this.state.text} onChange={this.handleTextChange} />
+            <label htmlFor="text">Pattern</label>
+            <input type="text" name="pattern" id="pattern" value={this.state.pattern} onChange={this.handlePatternChange} />
+            <input type="submit" id="submit" onClick={this.handleClick} />
+          </fieldset>
+        </form>
+        <div>{this.state.result}</div>
+      </div>
     );
   }
 }
--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import * as FuzzyMatchingJS from 'fuzzymatchingjs';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
